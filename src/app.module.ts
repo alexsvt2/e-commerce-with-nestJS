c6617e805dd6a073d2bbfa51f45afbdb7f296ebd
Waleedkhttab95 <wa.ez1995@gmail.com>
2021-03-04 21:24:40 +0300

    p

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_uRI),
     SharedModule,
     AuthModule,
