188bc8da12a8a3b7296854d059924175ada62efd
Waleedkhttab95 <wa.ez1995@gmail.com>
2021-03-05 18:31:20 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,19 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
     }),
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin@cluster0.4ieuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -24,4 +32,4 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
