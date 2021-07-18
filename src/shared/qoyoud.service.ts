e708f9260fddb8dbc29fd57cada8b9cf54fbf3e9
Waleedkhattab95 <85563112+Waleedkhattab95@users.noreply.github.com>
2021-07-18 17:32:44 +0300

    add qoyoud featuers

--- src/shared/qoyoud.service.ts
+++ src/shared/qoyoud.service.ts
@@ -115,7 +115,7 @@ export class QoyoudService {
     
   async convert(newDate){
     let current_datetime = new Date(newDate)
-    let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds();
+    let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate();
     return formatted_date;
   }
 }
